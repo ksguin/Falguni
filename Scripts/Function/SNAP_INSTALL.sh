2b73eb8d32715e8a6567e24af79c9e9e85969c0f
Kumar Subhodeep Guin <kumar.subhodeep64@gmail.com>
2020-09-19 13:42:13 +0530

    Simplified Snap installation process - Removed redundancy in snap package installation process - Updated necessary snap entries in install_software.sh

---
+++ Scripts/Function/SNAP_INSTALL.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#--How to USE--#
+# SNAP_INSTALL "<snap software installation code>" "Package Display name in UI" "<package name in snap list>"
+#--------------#
+
+SNAP_INSTALL() {
+	software=$1
+	Name=$2
+	snaplisting=$3
+	
+	#if already present, don't install
+	if [[ "$snaplisting" == $(snap list | awk {'print $1'} | grep $snaplisting) ]]; then
+						zenity --info --timeout 5\
+						--text="\n$Name Already Installed\t\t"\
+						--title "Installed" --no-wrap 2>/dev/null
+	else
+		sudo snap install $software 2>&1 | \
+		tee >( \
+		zenity --progress --pulsate --width=720\
+		--text="Downloading $Name..." --auto-kill --auto-close --no-cancel\
+		2>/dev/null)
+			
+		#Installation Complete Dialog
+		zenity --info --timeout 5\
+		--text="\nInstallation Complete\t\t"\
+		--title "$Name" --no-wrap 2>/dev/null
+	fi
+}
+
+					
+					
